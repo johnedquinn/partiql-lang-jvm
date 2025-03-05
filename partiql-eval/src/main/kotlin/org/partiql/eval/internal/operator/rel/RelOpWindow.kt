c50c540a99df71af8303b81959f8e6e2a97f1b88
John Ed Quinn <johqunn@amazon.com>
2025-03-05 15:20:09 -0800

    Working, though I know some things are wrong
    TODO: Flush out WindowPartition definition
    
    Flush out the orderingStart and orderingEnd

--- partiql-eval/src/main/kotlin/org/partiql/eval/internal/operator/rel/RelOpWindow.kt
+++ partiql-eval/src/main/kotlin/org/partiql/eval/internal/operator/rel/RelOpWindow.kt
@@ -26,6 +26,8 @@ internal class RelOpWindow(
     private var _orderingGroupEnd: Long = 0
     private var _partitionNumber: Long = 0
     private var _orderingGroupNumber: Long = 0
+    private var _partition: List<Row> = emptyList()
+    private var leftoverRow: Row? = null
 
     override fun openPeeking(env: Environment) {
         input.open(env)
@@ -33,31 +35,60 @@ internal class RelOpWindow(
         _currentPartition = emptyArray()
         _orderingGroupStart = 1 // TODO
         _orderingGroupEnd = 1 // TODO
+        _partitionNumber = -1L
         functions.map { it.reset(object : WindowPartition {}) }
     }
 
     override fun peek(): Row? {
-        if (input.hasNext().not()) {
-            return null
+        // Check if there is an existing partition. If so, evaluate and return.
+        _partitionNumber++
+        if (_partition.size > _partitionNumber) {
+            return produceResult()
         }
 
-        // Get the sorted input
-        val inputRow = input.next()
-        val newEnv = _env.push(inputRow)
-
-        // Determine if new partition
-        val inputPartition = Array(partitionBy.size) { partitionBy[it].eval(newEnv) }
-        val isNewPartition = comparator.compare(inputPartition, _currentPartition) != 0
-        if (isNewPartition) {
-            functions.map { it.reset(object : WindowPartition {}) } // Reset functions
-            _orderingGroupStart = 1 // TODO
-            _orderingGroupEnd = 1 // TODO
-            _currentPartition = inputPartition
+        // Create new partition's first row
+        _partitionNumber = 0L
+        val newPartition = mutableListOf<Row>()
+        val firstRow = when {
+            leftoverRow != null -> {
+                val tempRow = leftoverRow!!
+                leftoverRow = null
+                tempRow
+            }
+            else -> when (input.hasNext()) {
+                true -> input.next()
+                false -> return null
+            }
         }
+        newPartition.add(firstRow)
+        val newEnv = _env.push(firstRow)
+        val firstRowPartitionKeys = Array(partitionBy.size) { partitionBy[it].eval(newEnv) }
 
-        // Evaluate functions
+        // Add partition's remaining rows
+        while (input.hasNext()) {
+            val nextRow = input.next()
+            val nextEnv = _env.push(firstRow)
+            val nextPartitionKeys = Array(partitionBy.size) { partitionBy[it].eval(nextEnv) }
+            val isNewPartition = comparator.compare(firstRowPartitionKeys, nextPartitionKeys) != 0
+            if (isNewPartition) {
+                leftoverRow = nextRow
+                break
+            } else {
+                newPartition.add(nextRow)
+            }
+        }
+        _partition = newPartition
+        return produceResult()
+    }
+
+    /**
+     * This produces a result using the existing partition.
+     */
+    private fun produceResult(): Row {
+        val row = _partition[_partitionNumber.toInt()]
+        val newEnv = _env.push(row)
         val functions = functions.map { it.eval(newEnv, _orderingGroupStart, _orderingGroupEnd) }
-        val outputRow = inputRow.concat(Row.of(*functions.toTypedArray()))
+        val outputRow = row.concat(Row.of(*functions.toTypedArray()))
         return outputRow
     }
 
